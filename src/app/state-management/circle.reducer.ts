7be80cd15ab820e5fa01a9877e9f655195acc6dd
Marton Biro <marton.biro@mimacom.com>
2017-11-01 14:34:16 +0100

    fix typo

--- src/app/state-management/circle.reducer.ts
+++ src/app/state-management/circle.reducer.ts
@@ -1,14 +1,14 @@
 import { CircleState } from './circle.state';
 import { Actions, CIRCLE_MOVED, COLOR_CHANGED } from './circle.actions';
 
-const initalState: CircleState = {
+const initialState: CircleState = {
   x: 250,
   y: 250,
   color: 'red',
   colorCode: '#FF1C37'
 };
 
-export function circleReducer(state = initalState, action: Actions) {
+export function circleReducer(state = initialState, action: Actions) {
   let newState;
   switch (action.type) {
     case CIRCLE_MOVED:
