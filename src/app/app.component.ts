ae62650abe52301ec16fc9b032ba4bbc94c47dca
Marton Biro <marton.biro@mimacom.com>
2017-11-01 14:27:44 +0100

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>
+      <app-circle-box></app-circle-box>
+      <app-circle></app-circle>
+      <app-tracking-data></app-tracking-data>
+    </div>
+  `,
+  styles: [],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'app';
 }
--- src/app/state-management/circle.effects.ts
+++ src/app/state-management/circle.effects.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { Observable } from 'rxjs/Observable';
-import { Action } from '@ngrx/store';
 import {
   CHANGE_COLOR,
   CircleMovedAction,
@@ -21,23 +19,29 @@ import 'rxjs/add/operator/switchMap';
 export class CircleEffects {
 
   @Effect()
-  moveCircle$: Observable<Action> = this.actions$
+  moveCircle$ = this.actions$
     .ofType(MOVE_CIRCLE)
     .delay(200)
-    .map((action: MoveCircleAction) => new CircleMovedAction(action.payload));
+    .map((action: MoveCircleAction) =>
+      new CircleMovedAction(action.payload)
+    );
 
   @Effect()
-  changeColor$: Observable<Action> = this.actions$
+  changeColor$ = this.actions$
     .ofType(CHANGE_COLOR)
     .scan((acc: number) => acc + 1, 0)
     .map((count: number) => colors[count % 3])
-    .switchMap((color: string) => this.colorService
-      .getColorCode(color)
-      .map((code: string) => [color, code])
+    .switchMap((color: string) =>
+      this.colorService
+        .getColorCode(color)
+        .map((colorCode: string) => [color, colorCode])
     )
-    .map(([color, colorCode]) => new ColorChangedAction({ color, colorCode }));
+    .map(([color, colorCode]) =>
+      new ColorChangedAction({ color, colorCode })
+    );
 
 
-  constructor(private actions$: Actions, private colorService: ColorService) {
+  constructor(private actions$: Actions,
+              private colorService: ColorService) {
   }
 }
